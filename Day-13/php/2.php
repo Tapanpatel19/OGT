720552f2cc680a8635f6944aba5011b26125016b
2101031000177/Tapan <2101031000177@silveroakuni.ac.in>
2023-07-03 15:36:30 +0530

    Day-13 completed

---
+++ Day-13/php/2.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Tapan patel";
+$number = 2101031000177;
+$department = "B.tech IT";
+
+echo "Name: " . $name . "<br>";
+echo "Number: " . $number . "<br>";
+echo "Department: " . $department . "<br>";
+?>
